b69d246ea25b208bc026c659fa2ab327c76579f5
andrewlreeve <andrew@andrewlreeve.com>
2023-10-04 14:11:11 +1300

    fix: about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,31 +11,25 @@ import { SectionIntro } from '@/components/SectionIntro'
 import imageLeslieAlexander from '@/images/team/leslie-alexander.jpg'
 import imageMichaelFoster from '@/images/team/michael-foster.jpg'
 
-function Culture() {
+function Values() {
   return (
     <div className="mt-24 rounded-4xl bg-raspberry py-24 sm:mt-32 lg:mt-40 lg:py-32">
       <SectionIntro
-        eyebrow="Our culture"
-        title="Balance your passion with your passion for life."
+        eyebrow="Values"
+        title="What We Believe In"
         invert
       >
-        <p>
-          We are a group of like-minded people who share the same core values.
-        </p>
       </SectionIntro>
       <Container className="mt-16">
         <GridList>
-          <GridListItem title="Loyalty" invert>
-            Our team has been with us since the beginning because none of them
-            are allowed to have LinkedIn profiles.
+          <GridListItem title="Honesty" invert>
+            We’re straight talking and open with communication. 
           </GridListItem>
-          <GridListItem title="Trust" invert>
-            We don’t care when our team works just as long as they are working
-            every waking second.
+          <GridListItem title="Action" invert>
+            We have a strong bias for action and making things happen. 
           </GridListItem>
-          <GridListItem title="Compassion" invert>
-            You never know what someone is going through at home and we make
-            sure to never find out.
+          <GridListItem title="Collaboration" invert>
+            We love to become a trusted extension of your team.
           </GridListItem>
         </GridList>
       </Container>
@@ -121,22 +115,28 @@ export default async function About() {
     <>
       <PageIntro eyebrow="About" title="Who we are">
         <p>
-          Pep Zest is a boutique online and e-commerce growth consultancy founded by Annabel Causer and Andrew Reeve, each with over 15 years experience across growth, marketing, technology, ecommerce and data.
+          Pep Zest is a boutique online and e-commerce growth consultancy founded by Annabel Causer and Andrew Reeve,
+          each with over 15 years experience across growth, marketing, technology, ecommerce and data.
         </p>
         <div className="mt-10 max-w-2xl space-y-6 text-base">
           <p>
-            “Pep and Zest is built to provide the support we wanted when we were running our own direct-to-consumer business. Our approach is to be an extension of your team, to understand your business levers and the actions required to drive sustainable long-term growth. We’ll help to dial up your growth, embed the skills you need to maintain momentum, and then get out your way”.
+            “Pep and Zest is built to provide the support we wanted when we were running our own direct-to-consumer
+            business. Our approach is to be an extension of your team, to understand your business levers and the 
+            actions required to drive sustainable long-term growth. We’ll help to dial up your growth, embed the skills you need to maintain momentum, and then get out your way”.
           </p>
           <p>
             Annabel and Andrew mentor and advise startups across NZ, AU and UK and are driven to help companies to discover what will move their online businesses forward.
           </p>
           <p>
-            Prior to founding Pep and Zest, Annabel and Andrew founded a VC-backed online craft beer retailer in the UK. Highlights include driving 3 x yearly customer and revenue growth; VC, crowd and angel investment; 31% awareness among craft beer drinkers; and multiple start up awards including #49 StartUp100 Awards, Future 50 Disruptive Businesses, Retail Business of the Year Finalist and The Guardian Startup of the Year Finalist.
+            Prior to founding Pep and Zest, Annabel and Andrew founded a VC-backed online craft beer retailer in the UK. 
+            Highlights include driving 3 x yearly customer and revenue growth; VC, crowd and angel investment; 31% awareness
+            among craft beer drinkers; and multiple start up awards including #49 StartUp100 Awards, Future 50 Disruptive Businesses,
+            Retail Business of the Year Finalist and The Guardian Startup of the Year Finalist.
           </p>
         </div>
       </PageIntro>
 
-      <Culture />
+      <Values />
 
       <Team />
 
