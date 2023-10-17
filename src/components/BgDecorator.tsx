2f5c1199bc07568393dc16045646749c0c3602f3
Tuncay Kaplan <tkaplan@innova.com.tr>
2023-10-18 02:51:42 +0300

    fix bg decorator style

--- src/components/BgDecorator.tsx
+++ src/components/BgDecorator.tsx
@@ -3,19 +3,25 @@ type BgDecoratorProps = React.HTMLAttributes<HTMLElement> & {
 }
 
 const BgDecorator: React.FC<BgDecoratorProps> = ({ type, ...restProps }) => {
-  const srcObject: Record<string, { src: string; class?: string }> = {
+  const srcObject: Record<
+    string,
+    { src: string; class?: string; wrapperClass?: string }
+  > = {
     top: { src: 'images/ui/pz_background_top.png' },
     middle: {
       src: 'images/ui/pz_background_middle.png',
-      class: 'mt-0 desktopxl:justify-start desktopxl:left-0',
+      class: 'mt-0',
+      wrapperClass: 'desktopxl:justify-start desktopxl:left-0',
     },
     bottom: { src: 'images/ui/pz_background_bottom.png' },
   }
 
-  const { src, class: additionalClass } = srcObject[type]
+  const { src, class: additionalClass, wrapperClass } = srcObject[type]
   return (
-    <div className="min-w-8xl absolute -z-10 w-8xl" {...restProps}>
-      <div className="align-center relative left-bg-center-fix flex justify-end">
+    <div className="min-w-8xl absolute -z-10 w-full" {...restProps}>
+      <div
+        className={`align-center relative left-bg-center-fix flex justify-end ${wrapperClass}`}
+      >
         <img src={src} className={`-mt-92px max-w-none ${additionalClass}`} />
       </div>
     </div>
