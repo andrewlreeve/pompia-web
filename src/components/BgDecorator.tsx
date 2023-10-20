07a619afbe16888e02da1e89b4b38d1ed6e81476
Tuncay Kaplan <tkaplan@innova.com.tr>
2023-10-20 08:40:55 +0300

    replace <img> element w/ Next/Image

--- src/components/BgDecorator.tsx
+++ src/components/BgDecorator.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 type BgDecoratorProps = React.HTMLAttributes<HTMLElement> & {
   type: 'top' | 'middle' | 'bottom'
 }
@@ -5,15 +6,33 @@ type BgDecoratorProps = React.HTMLAttributes<HTMLElement> & {
 const BgDecorator: React.FC<BgDecoratorProps> = ({ type, ...restProps }) => {
   const srcObject: Record<
     string,
-    { src: string; class?: string; wrapperClass?: string }
+    {
+      src: string
+      width: number
+      height: number
+      class?: string
+      wrapperClass?: string
+      sizes?: string
+    }
   > = {
-    top: { src: 'images/ui/pz_background_top.png' },
+    top: {
+      src: '/images/ui/pz_background_top.png',
+      width: 1504,
+      height: 856,
+    },
     middle: {
-      src: 'images/ui/pz_background_middle.png',
+      src: '/images/ui/pz_background_middle.png',
+      width: 1920,
+      height: 1165,
       class: 'mt-0',
       wrapperClass: 'desktopxl:justify-start desktopxl:left-0',
+      sizes: '(min-width: 1024px) 41rem, 31rem',
     },
-    bottom: { src: 'images/ui/pz_background_bottom.png' },
+    bottom: {
+      src: '/images/ui/pz_background_bottom.png',
+      width: 1916,
+      height: 939,
+    },
   }
 
   const {
@@ -26,7 +45,14 @@ const BgDecorator: React.FC<BgDecoratorProps> = ({ type, ...restProps }) => {
       <div
         className={`align-center relative left-bg-center-fix flex justify-end ${wrapperClass}`}
       >
-        <img src={src} className={`-mt-92px max-w-none ${additionalClass}`} />
+        <Image
+          alt=""
+          width={srcObject[type].width}
+          height={srcObject[type].height}
+          src={src}
+          sizes={srcObject[type].sizes}
+          className={`-mt-92px max-w-none ${additionalClass}`}
+        />
       </div>
     </div>
   )
